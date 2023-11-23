133fcea0b8c08bcc0f66213a41d0af02476f6fb8
Vlada Rozova <vladarozova@vladas-mbp.mobility.unimelb.net.au>
2023-11-23 13:32:24 +1100

    Tuck away function for parsing annotation files

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 def get_filename(patient_id, report_no, file_format='ann'):
     """
@@ -22,6 +23,215 @@ def read_report(x, path):
     
     return text
 
+
+
+def parse_ann_files(df, path):
+    """
+    Parse .ann files and write into a dataframe of gold standard concepts.
+    """
+    # Create dataframes to store annotations
+    concepts = pd.DataFrame(columns=['histopathology_id', 'patient_id', 'report_no', 
+                                     'concept_id', 'concept', 'phrase', 'position', 'start_char', 'end_char'])
+    relations = pd.DataFrame(columns=['histopathology_id', 'patient_id', 'report_no', 
+                                      'relation_id', 'relation', 'arg1', 'arg2'])
+
+    for _, x in df.iterrows():
+        # Define filename
+        filename = get_filename(x.patient_id, x.report_no, file_format='ann')
+
+        # Open and read annotation file
+        with open(path + "annotations/" + filename, 'r') as f:
+            annotation = f.readlines()
+
+        if annotation:    
+            # Loop over each line of the annotation file
+            for line in annotation:
+
+                # Concept
+                if re.match("T", line):
+
+                    # Create an entry containing concept ID, category, position and the raw text
+                    substrings = line.strip().split('\t')
+                    concept_id = substrings[0]
+                    concept = substrings[1].split(maxsplit=1)[0]
+                    position = substrings[1].split(maxsplit=1)[1]
+                    start_char, end_char = re.split(' |;', position)[-2:]
+                    text = substrings[2]
+
+                    tmp = pd.DataFrame({
+                        'histopathology_id': x.histopathology_id,
+                        'patient_id': x.patient_id, 
+                        'report_no': x.report_no, 
+                        'concept_id': concept_id, 
+                        'concept': concept, 
+                        'phrase': text,
+                        'position': position, 
+                        'start_char': int(start_char),
+                        'end_char': int(end_char),
+                    }, index=[0])
+
+                    # Add to the table of concepts
+                    concepts = pd.concat([concepts, tmp], axis=0, ignore_index=True)
+
+                # Relation
+                elif re.match("R", line):
+
+                    # Create an entry containing relation ID, type and IDs of the arguments
+                    substrings = line.strip().split()
+                    relation_id = substrings[0]
+                    relation = substrings[1]
+                    arg1 = substrings[2].split(':')[1]
+                    arg2 = substrings[3].split(':')[1]
+
+                    tmp = pd.DataFrame({
+                        'histopathology_id': x.histopathology_id,
+                        'patient_id': x.patient_id, 
+                        'report_no': x.report_no, 
+                        'relation_id': relation_id, 
+                        'relation': relation, 
+                        'arg1': arg1, 
+                        'arg2': arg2
+                    }, index=[0])
+
+                    # Add to the table of relations
+                    relations = pd.concat([relations, tmp], axis=0, ignore_index=True)
+
+    # Convert patient ID and report number to int
+    concepts[['patient_id', 'report_no']] = concepts[['patient_id', 'report_no']].astype(int)
+    relations[['patient_id', 'report_no']] = relations[['patient_id', 'report_no']].astype(int)
+
+#     print("Extracted %d concepts and %d relations." % (concepts.shape[0], relations.shape[0]))
+    
+    return concepts, relations
+
+
+def handle_discont_concepts(concepts):
+    """
+    Split discontinuos concepts.
+    """
+    # Discont concepts have ;-separated positions
+    idx = concepts[concepts.position.str.contains(";")].index
+
+    # Split discont concepts into a separate dataframe
+    discont = concepts.iloc[idx].copy()
+    concepts.drop(idx, inplace=True)
+    
+    # Loop over discont concepts extracting individual spans
+    for _,x in discont.iterrows():
+        spans = []
+        i = 0
+        for pos in x.position.split(';'):
+            # Extract start and end char positions
+            start_char, end_char = map(int, pos.split())
+            # Calculate span length
+            len_span = end_char - start_char
+            # Extract span text
+            phrase = x.phrase[i:i+len_span]
+            # Add to list of spans
+            spans.append((start_char, end_char, phrase))
+            i = i + len_span + 1
+
+        # Sort extracted spans by starting position
+        spans = sorted(spans, key=lambda x: x[0])
+
+        # Append extracted spans to the dataframe with gold standard concepts 
+        for span in spans:
+            tmp = x.copy()
+            tmp['start_char'] = span[0]
+            tmp['end_char'] = span[1]
+            tmp['phrase'] = span[2]
+            concepts = pd.concat([concepts, tmp.to_frame().T], axis=0, ignore_index=True)
+
+    # Remove position column
+    concepts.drop('position', axis=1, inplace=True)
+    
+#     print("After handling discontinous concepts there are a total of %d concepts." % concepts.shape[0])
+    
+    return concepts
+
+
+def get_cue_order(concepts, relations, cues_to_check):
+    """
+    Check if a cue is preceeding or following. 
+    """
+    def assign_termset(x):
+        arg2_ids = relations[(relations.histopathology_id==x.histopathology_id) & 
+                             (relations.arg1==x.concept_id)
+                            ].arg2
+        arg2_start_char = concepts[(concepts.histopathology_id==x.histopathology_id) & 
+                                    concepts.concept_id.isin(arg2_ids)
+                                   ].start_char
+        return (x.start_char < arg2_start_char).any(), (x.start_char > arg2_start_char).any()
+
+    # Only check order for positive and negative cue
+    cues = concepts[concepts.concept.isin(cues_to_check)]
+    
+    # Determine if a cue is preceding and/or following
+    return pd.DataFrame(cues.apply(assign_termset, axis=1).tolist(), 
+                                                        index=cues.index)
+
+
+def add_composite_concepts(concepts, relations, relations_to_add):
+    """
+    Combine concepts and relations into composite concepts.
+    """
+    def get_composite_concept(x, name):
+        # Define the next vacant concept ID
+        next_id = concepts[concepts.histopathology_id==x.histopathology_id].concept_id.apply(lambda x: 
+                                                                                             int(x[1:])
+                                                                                            ).max() + 1
+        # Determine the object (Arg2) of a relation
+        y = concepts[(concepts.histopathology_id==x.histopathology_id) & 
+                     (concepts.concept_id==x.arg2)].iloc[0]
+            
+        # Create an entry containing concept ID, composite category, position and the raw text
+        return pd.DataFrame({'histopathology_id': x.histopathology_id,
+                             'patient_id': x.patient_id,
+                             'report_no': x.report_no, 
+                             'concept_id': 'T' + str(next_id), 
+                             'concept': name + y.concept,
+                             'phrase': y.phrase,
+                             'start_char': y.start_char,
+                             'end_char': y.end_char,
+                            }, index=[0])
+            
+    for k,v in relations_to_add.items():
+        # Loop over the dataframe with extracted relations
+        for _, x in relations[relations.relation==k].iterrows():
+            # Add to the table of concepts
+            concepts = pd.concat([concepts, get_composite_concept(x, v)], axis=0, ignore_index=True)
+    
+     # Drop duplicated composite concepts
+    concepts.drop_duplicates(subset=['histopathology_id', 'concept', 'start_char'], inplace=True, ignore_index=True)
+
+#     print("Totalling %d concepts and composite concepts." % concepts.shape[0])
+        
+    return concepts
+
+
+
+def read_annotations(df, path):
+    # Parse annotation files
+    concepts, relations = parse_ann_files(df, path)
+    
+    # Separate discontinuous concepts
+    concepts = handle_discont_concepts(concepts)
+    
+    # Preceding and following termsets
+    concepts[['preceding', 'following']] = get_cue_order(concepts, relations, 
+                                                         ['positive', 'negative'])
+    
+    # Save the extracted concepts and relations
+    concepts.to_csv("../datasets/gold_concepts.csv", index=False)
+    relations.to_csv("../datasets/gold_relations.csv", index=False)
+    return concepts
+    
+    # Create composite concepts
+    concepts = add_composite_concepts(concepts, relations, 
+                                      {'positive-rel': 'affirmed', 'negative-rel': 'negated'})
+    
+    # Save the extracted annotations
+    concepts.to_csv("../datasets/gold_composite.csv", index=False)
 
 
 def get_neighbors(doc, i):
